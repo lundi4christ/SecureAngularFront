0f53336ab987f143e17c1c14664a19c628efea96
lundi4christ <lundi4christ@gmail.com>
2023-09-14 08:47:53 +0100

    7commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {PanelboxComponent} from './panelbox/panelbox.component';
+import {FormsComponent} from './forms/forms.component';
+import {DatatableviewComponent} from './datatableview/datatableview.component';
+
+
+const routes: Routes = [
+  {path: '', component: PanelboxComponent, data: {title: 'Dashboard'}},
+  {path: 'forms', component: FormsComponent, data: {title: 'Signup'}},
+  {path: 'users', component: DatatableviewComponent, data: {title: 'User Details'}}
+];
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
