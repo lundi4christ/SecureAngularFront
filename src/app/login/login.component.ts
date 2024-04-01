1a4314d2d5c8613fec9ca77d94ab5de93129a88d
lundi4christ <lundi4christ@gmail.com>
2024-04-01 16:49:03 +0100

    logincommit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {LoginService} from '../services/login.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private loginService: LoginService, private router: Router) { }
   backgroundImage = 'assets/mylogin.jpg';
+  credentials = {
+    usernameOrEmail: '',
+    password: ''
+  };
+
   ngOnInit(): void {
+  }
+
+  // tslint:disable-next-line:typedef
+  login(){
+    this.loginService.loginUser(this.credentials).subscribe(
+    response => {
+      this.router.navigate(['/home']);
+    },
+      /*error => {
+      this.error = 'Invalid username or password';
+      }*/
+    );
   }
 
 }
